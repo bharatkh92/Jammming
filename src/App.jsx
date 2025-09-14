05886a19f5ba24cde806a19c0d3b95dcb000bfe4
bharatkh92 <bharatkh92@gmail.com>
2025-09-14 17:19:13 +0530

    accessing spotifytoken after render in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,46 @@
-import { useState } from 'react';
-import './App.css';
-import PlaylistContainer from './containers/PlaylistContainer';
-import SearchBarContainer from './containers/SearchBarContainer';
-import SearchResultsContainer from './containers/SearchResultsContainer';
-import {ResponseData, PlaylistData} from './MockData';
-import { redirectToAuthCodeFlow } from './authCodeWithPkce';
+import { useEffect, useState } from "react";
+import "./App.css";
+import PlaylistContainer from "./containers/PlaylistContainer";
+import SearchBarContainer from "./containers/SearchBarContainer";
+import SearchResultsContainer from "./containers/SearchResultsContainer";
+import { ResponseData, PlaylistData } from "./MockData";
+import { redirectToAuthCodeFlow } from "./authCodeWithPkce";
 
 const clientId = "66658c358a2d4036983a5e036dad9f41";
 const redirectURI = "https://codecademyjammingbharatkh92.netlify.app/callback";
-let spotify_access_token = localStorage.getItem('spotify_access_token');
-
-if(!spotify_access_token) {
-    redirectToAuthCodeFlow(clientId, redirectURI);
-    console.log(spotify_access_token)
-} else {
-  console.log("token is successfully stored")
-  console.log(spotify_access_token)
-}
-
 
 function App() {
   const [response, setResponse] = useState();
   const [playlist, setPlaylist] = useState(PlaylistData);
 
+  useEffect(() => {
+    let spotify_access_token = localStorage.getItem("spotify_access_token");
+
+    if (!spotify_access_token) {
+      redirectToAuthCodeFlow(clientId, redirectURI);
+      console.log(spotify_access_token);
+    } else {
+      console.log("token is successfully stored");
+      console.log(spotify_access_token);
+    }
+  }, []);
+
   return (
-    <div className='app'>
-      <div className='searchBarContainer'>
-      <SearchBarContainer setResponse={setResponse} />
+    <div className="app">
+      <div className="searchBarContainer">
+        <SearchBarContainer setResponse={setResponse} />
       </div>
-      <div className='playlistSearchResultsContainer'>
+      <div className="playlistSearchResultsContainer">
         <PlaylistContainer playlist={playlist} setPlaylist={setPlaylist} />
-        <SearchResultsContainer response={response} setResponse={setResponse} playlist={playlist} setPlaylist={setPlaylist} />
+        <SearchResultsContainer
+          response={response}
+          setResponse={setResponse}
+          playlist={playlist}
+          setPlaylist={setPlaylist}
+        />
       </div>
-      
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
